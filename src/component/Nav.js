1bea42eb1f7c452fc25d5ae99aae7f5cbf668072
maytham mahdi <maytham.mahdi@codeforiraq.org>
2020-01-24 02:27:26 +0300

    error

--- src/component/Nav.js
+++ src/component/Nav.js
@@ -1,39 +1,44 @@
 import React from "react";
 import { BrowserRouter, Route, Link, Switch } from "react-router-dom";
-const BtnNav = props => {
-  return (
-    <BrowserRouter>
-      {props.bntInfo.map(bntInfo => {
-        <div>
-          <Link to={bntInfo.path}>{bntInfo.text + "   "}</Link> ;
-          <Route
-            path={bntInfo.path}
-            exact
-            component={() => {
-              console.log("suc");
-            }}
-          />
-          ;
-        </div>;
-      })}
-      )
-    </BrowserRouter>
-  );
-};
 
-export default class Nav extends React.Component {
-  render() {
+const LinkPush = props => (
+  <Link to={props.btnInfo.path}>{props.btnInfo.text + "   "}</Link>
+);
+
+const RoutePush = props => (
+  <Route
+    path={props.btnInfo.path}
+    exact
+    component={() => {
+      console.log("suc");
+    }}
+  />
+);
+
+const BtnNav = props => (
+  <BrowserRouter>
+    
+      {props.btnInfo.map(btnInfo => {
+        return <LinkPush btnInfo={btnInfo} />;
+      })}
+      {props.btnInfo.map(btnInfo => {
+        return <RoutePush btnInfo={btnInfo} />;
+      })}
+  
+  </BrowserRouter>
+);
+
+export default () => {
+  
     return (
-      <span>
-        <BtnNav
-          bntInfo={[
-            { text: "Home", path: "/", funName: "name" },
-            { text: "Trend", path: "trend", funName: "name" },
-            { text: "Upload", path: "upload", funName: "name" },
-            { text: "Search", path: "search", funName: "name" }
-          ]}
-        />
-      </span>
+      <BtnNav
+        btnInfo={[
+          { text: "Home", path: "/", funName: "name" },
+          { text: "Trend", path: "trend", funName: "name" },
+          { text: "Upload", path: "upload", funName: "name" },
+          { text: "Search", path: "search", funName: "name" }
+        ]}
+      />
     );
-  }
+  
 }
