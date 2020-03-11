5e22234a5a229ff7a80a3887cb4eee3ae9278c5a
maytham mahdi <maytham.mahdi@codeforiraq.org>
2020-03-11 15:14:19 +0300

    add upload page style

--- src/Pages/Upload/Upload.js
+++ src/Pages/Upload/Upload.js
@@ -1,11 +1,35 @@
-import React from "react" ;
+import React from "react";
 
-export default class Upload extends React.Component{
-render(){
-    return <div>
-        <form>
-            
-        </form>
-    </div>
-}
+export default class Upload extends React.Component {
+     previewFile = () => {
+        var preview = document.querySelector('img');
+        var file    = document.querySelector('input[type=file]').files[0];
+        var reader  = new FileReader();
+      
+        reader.onloadend = function () {
+          preview.src = reader.result;
+        }
+      
+        if (file) {
+          reader.readAsDataURL(file);
+        } else {
+          preview.src = "";
+        }
+      }
+
+    render() {
+        return (
+            <div className="upload">
+                <form>
+
+                    <input className="publisher-name input-text" placeholder="اسم الناشر"></input>
+                    <textarea className="paragraph input-text"  placeholder="النص"></textarea>
+                    <input className="hashTag input-text" placeholder="هاشتاك#"></input>
+                    <input className="image-upload " type="file" name="myImage" accept="image/*" onChange={this.previewFile} ></input>
+                    <img className="image" src="de-img.png"></img>
+                    <input className="btn-uploads" type="submit" value="نشر"></input>
+                </form>
+            </div>
+        );
+    }
 }
